2f785a06c5d723dafff87091ef59ce618fbd5e59
Rabin Avidan <rabin.avidan.dev@gmail.com>
2026-07-15 13:50:09 +0300

    Initial Commit

---
+++ rabin-task-manager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  rabin-task-manager
+//
+//  Created by root26 on 15/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ rabin-task-manager/rabin_task_managerApp.swift
@@ -0,0 +1,17 @@
+//
+//  rabin_task_managerApp.swift
+//  rabin-task-manager
+//
+//  Created by root26 on 15/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct rabin_task_managerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
